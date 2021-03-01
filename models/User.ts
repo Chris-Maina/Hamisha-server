758a7a60b7e3a21e5e68d76ab2beccc926d15135
Chris Maina <mainachrisw@gmail.com>
2021-03-01 12:52:35 +0300

    feat: add Customer and Model relationMappings

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,9 @@
 import bcrypt from 'bcrypt';
 import { Model } from 'objection';
 
+import Mover from './Mover';
+import Customer from './Customer';
+
 class User extends Model {
   id!: number
   email!: string
@@ -8,6 +11,8 @@ class User extends Model {
   last_name!: string
   password!: string
   location!: string
+  customer!: Customer
+  mover!: Mover
 
   static get tableName() {
     return 'users'
@@ -19,6 +24,29 @@ class User extends Model {
 
   static hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, 10)
+  }
+
+  static get relationMappings() {
+    return {
+      customer: {
+        relation: Model.HasOneRelation,
+        modelClass: Customer,
+        filter: (query: any) => query.select('id', 'created_at'),
+        join: {
+          from: 'users.id',
+          to: 'customers.user_id'
+        }
+      },
+      mover: {
+        relation: Model.HasOneRelation,
+        modelClass: Mover,
+        filter: (query: any) => query.select('id', 'created_at', 'description'),
+        join: {
+          from: 'users.id',
+          to: 'movers.user_id'
+        }
+      },
+    }
   }
 }
 
