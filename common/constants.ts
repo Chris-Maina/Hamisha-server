106fb10527872aef1d603823ada25562ec6f0e5d
Chris Maina <mainachrisw@gmail.com>
2021-03-25 07:40:25 +0300

    chore: add PROPOSAL_STATUS constant

--- common/constants.ts
+++ common/constants.ts
@@ -2,3 +2,14 @@ export const USER_TYPES = {
   CUSTOMER: 'customer',
   MOVER: 'mover'
 }
+
+export const PROPOSAL_STATUS = {
+  SENT: 'proposal sent',
+  WITHDRAW: 'proposal withdrawn',
+  REJECTED: 'proposal rejected',
+  ACCEPTED: 'proposal accepted',
+  NEGOTIATION: 'negotiation phase',
+  JOB_START: 'job started',
+  JOB_SUCCESS: 'job finished(successfully)',
+  JOB_UNSUCCESS: 'job finished(unsuccessfully)'
+};
