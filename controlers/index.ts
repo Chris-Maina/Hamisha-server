b1bef5e759f71a646428dfc4d488b2713f5773a7
Chris Maina <mainachrisw@gmail.com>
2021-03-25 07:40:59 +0300

    feat: register proposal routes

--- controlers/index.ts
+++ controlers/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import jobRoutes from './jobs.controlers';
 import authRoutes from './auth.controlers';
 import userRoutes from './users.controlers';
+import proposalRoutes from './proposal.controllers';
 import paymentTypeRoutes from './payment_types.controllers';
 
 const router = Router();
 router.use(authRoutes);
 router.use(userRoutes);
 router.use('/jobs', jobRoutes);
-router.use('/payment_types', paymentTypeRoutes)
+router.use('/payment_types', paymentTypeRoutes);
+router.use('/proposals', proposalRoutes);
 
 export default router;
