71817091dd8963a185c95f3e596c3f003f624677
Chris Maina <mainachrisw@gmail.com>
2021-04-03 09:18:02 +0300

    chore: export Room, Message models

--- models/index.ts
+++ models/index.ts
@@ -1,6 +1,8 @@
 export { default as Job } from './Job';
 export { default as User } from './User';
+export { default as Room } from './Room';
 export { default as Mover } from './Mover';
+export { default as Message } from './Message';
 export { default as Proposal } from './Proposal';
 export { default as Customer } from './Customer';
 export { default as PaymentType } from './PaymentType';
