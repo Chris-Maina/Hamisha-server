c6306ad5dc0f9877e6c295c721f0c0c7840a221d
Chris Maina <mainachrisw@gmail.com>
2021-04-03 09:16:23 +0300

    chore: add event constants

--- common/constants.ts
+++ common/constants.ts
@@ -13,3 +13,11 @@ export const PROPOSAL_STATUS = {
   JOB_SUCCESS: 'job finished(successfully)',
   JOB_UNSUCCESS: 'job finished(unsuccessfully)'
 };
+export const PRIVATE_MESSAGE = 'events/PRIVATE_MESSAGE';
+export const CREATE_ROOM = 'events/CREATE_ROOM';
+export const ADD_ROOM_MESSAGE = 'events/ADD_ROOM_MESSAGE';
+export const MESSAGE = 'events/MESSAGE';
+export const SOCKET_ERRORS = 'events/SOCKET_ERRORS';
+export const LOAD_MESSAGES_IN_ROOM = 'events/LOAD_MESSAGES_IN_ROOM';
+export const FETCH_USER_ROOMS = 'events/FETCH_USER_ROOMS';
+export const FETCH_USER_ROOMS_RESPONSE = 'events/FETCH_USER_ROOMS_RESPONSE';
