fd79a531af09d0ad2115436baf917d19e9339852
Chris Maina <mainachrisw@gmail.com>
2021-04-06 22:15:59 +0300

    feat: add more socket events
     - add JOIN_ROOM event
     - add PARTICIPANTS_JOIN_ROOM event
     - add ADD_ROOM_MESSAGE event

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
 import morgan from 'morgan';
+import EventEmitter from "events";
 import { Model } from 'objection';
 import { Server, Socket } from "socket.io";
 import { createServer } from "http";
@@ -13,14 +14,16 @@ import routes from './controlers';
 import knex from './knex';
 import {
   MESSAGE,
+  JOIN_ROOM,
   CREATE_ROOM,
   SOCKET_ERRORS,
-  PRIVATE_MESSAGE,
+  FETCH_USER_ROOMS,
   ADD_ROOM_MESSAGE,
   LOAD_MESSAGES_IN_ROOM,
-  FETCH_USER_ROOMS
+  PARTICIPANTS_JOIN_ROOM,
+  FETCH_MESSAGES_FROM_ROOM,
 } from './common/constants';
-import { Message, Room } from './models';
+import { Message, Participants, Room } from './models';
 
 // Bind all Models to a knex instance.
 Model.knex(knex);
@@ -60,32 +63,16 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
 const PORT = process.env.PORT || 3100;
 
 io.on("connection", (socket: Socket) => {
+  const socketIOEE = new EventEmitter();
   console.log('Connected to web socket successfully');
 
-  // Handle rooms request
-  socket.on(FETCH_USER_ROOMS, async ({ user_id }) => {
+  // Handle creating a room with participants
+  socket.on(CREATE_ROOM, async ({ name, participants }) => {
     try {
-      const response = await Room.query().where("user_id", user_id)
-    } catch (error) {
-      // add error handler that conforms to the error midleware
-      socket.emit(SOCKET_ERRORS, error);
-    }
-  });
-
-  // Handle room creation
-  socket.on(CREATE_ROOM, async ({ name }) => {
-    // API request to create a room
-    try {
-      let response = await Room.query().findOne({ name });
-
-      if (!response) {
-        response = await Room.query().insert({ name });
-      } else {
-        // emit load messages
-        const messages = await Message.query().where('room_id', response.id);
-        socket.emit(LOAD_MESSAGES_IN_ROOM, messages);
-      }
+      const response = await Room.query().insert({ name });
       // add participants
+      socketIOEE.emit(PARTICIPANTS_JOIN_ROOM, { room_id: response.id, participants });
+  
       socket.join(response.id.toString());
       socket.emit(CREATE_ROOM, response);
     } catch (error) {
@@ -93,27 +80,63 @@ io.on("connection", (socket: Socket) => {
     }
   });
 
-  // Handle fetching a room
-  // Handle leave room/switch rooms
-
-  // Handle private messages
-  socket.on(PRIVATE_MESSAGE, async (anotherSocketId, msg) => {
-    socket.to(anotherSocketId).emit(PRIVATE_MESSAGE, socket.id, msg);
-  });
-
-  // Handle room messages
+  // Handle adding message(s) to a room
   socket.on(ADD_ROOM_MESSAGE, async ({ roomId, msg, sender }) => {
     try {
       const response = await Message.query().insert({
         text: msg,
-        room_id: roomId,
-        user_id: sender.id
+        room_id: roomId,   // sent message to
+        user_id: sender.id // sent message from
       });
+
 
       io.to(roomId.toString()).emit(MESSAGE, response);
     } catch (error) {
       socket.emit(SOCKET_ERRORS, error);
     }
+  });
+
+  // Handle fetching messages from a room
+  socket.on(FETCH_MESSAGES_FROM_ROOM, async ({ room_id }) => {
+    try {
+      const response = await Message
+        .query()
+        .where('room_id', room_id)
+
+      socket.emit(LOAD_MESSAGES_IN_ROOM, response);
+    } catch (error) {
+      // add error handler that conforms to the error midleware
+      socket.emit(SOCKET_ERRORS, error);
+    }
+  });
+
+  // Handle rooms request on connect
+  socket.on(FETCH_USER_ROOMS, async ({ user_id }) => {
+    try {
+      const response = await Room.query().where("user_id", user_id);
+    } catch (error) {
+      // add error handler that conforms to the error midleware
+      socket.emit(SOCKET_ERRORS, error);
+    }
+  });
+
+  // Listen to PARTICIPANTS_JOIN_ROOM events and handle participants joining a room
+  socketIOEE.on(PARTICIPANTS_JOIN_ROOM, async ({ room_id, participants }) => {
+    const joinRequests = participants.map(async (user_id: number) => {
+      const hasJoined = await Participants.query().findOne({ room_id, user_id });
+      if (hasJoined) return hasJoined;
+      const participant = await Participants.query().insert({ user_id, room_id });
+      return participant;
+    });
+    await Promise.all(joinRequests);
+  });
+
+  // Handle fetching a room
+  // Handle leave room/switch rooms
+
+  // Handle joining room
+  socket.on(JOIN_ROOM, ({ room_id }) => {
+    socket.join(room_id.toString());
   });
 });
 
