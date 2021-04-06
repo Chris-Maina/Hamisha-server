832ba292d9cd07397fd7d1c138917ee65d56484c
Chris Maina <mainachrisw@gmail.com>
2021-04-06 22:51:07 +0300

    chore: modify Room model to add relationMappings

--- models/Room.ts
+++ models/Room.ts
@@ -1,4 +1,5 @@
 import { Model } from "objection";
+import Participants from './Participants';
 
 class Room extends Model {
   id!: number
@@ -7,6 +8,19 @@ class Room extends Model {
   static get tableName() {
     return "rooms";
   }
+
+  static get relationMappings() {
+    return {
+      participants: {
+        relation: Model.HasManyRelation,
+        modelClass: Participants,
+        join: {
+          from: "rooms.id",
+          to: "participants.room_id"
+        },
+      },
+    };
+  };
 }
 
 export default Room;
