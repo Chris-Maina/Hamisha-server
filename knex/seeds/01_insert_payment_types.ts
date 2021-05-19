715d7be784205b575b4d62deb3f67bfca5dc921b
Chris Maina <mainachrisw@gmail.com>
2021-05-19 09:12:59 +0300

    bug: use ALTER SEQUENCE command to auto increment

--- knex/seeds/01_insert_payment_types.ts
+++ knex/seeds/01_insert_payment_types.ts
@@ -3,7 +3,7 @@ import * as Knex from "knex";
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("payment_types").del();
-    await knex.raw("ALTER TABLE payment_types AUTO_INCREMENT = 1;")
+    await knex.raw("ALTER SEQUENCE payment_types_id_seq RESTART WITH 1")
 
     // Inserts seed entries
     await knex("payment_types").insert([
