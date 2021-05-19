8962349ac8fe1d34b45a3243be1e2378e7d8bee8
Chris Maina <mainachrisw@gmail.com>
2021-05-19 08:51:01 +0300

    bug: modify to use del() and raw() functions to delete entries

--- knex/seeds/01_insert_payment_types.ts
+++ knex/seeds/01_insert_payment_types.ts
@@ -2,7 +2,8 @@ import * as Knex from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("payment_types").truncate();
+    await knex("payment_types").del();
+    await knex.raw("ALTER TABLE payment_types AUTO_INCREMENT = 1")
 
     // Inserts seed entries
     await knex("payment_types").insert([
