88e8b5efeb9617bbb89280b50677bf456af98b56
Chris Maina <mainachrisw@gmail.com>
2021-11-09 22:14:15 +0300

    chore: moify register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,11 +7,14 @@ export const loginSchema = Joi.object({
 });
 
 export const registerSchema = loginSchema.keys({
-  first_name: Joi.string().required(),
-  last_name: Joi.string().required(),
+  first_name: Joi.string().optional().allow(''),
+  last_name: Joi.string().optional().allow(''),
   type: Joi.string().required(),
   description: Joi.string().optional().allow(''), // empty strings are not allowed by default and must be enabled with allow('')
-  phone_number: Joi.string().optional()
+  phone_number: Joi.string().optional(),
+  reg_number: Joi.string().optional(),
+  vehicle_pic: Joi.string().optional().allow(''),
+  vehicle_type: Joi.string().optional().allow(''),
 });
 
 export const proposalSchema = Joi.object({
