4f4864e55ea0bb10c332f556237e326aa4657d22
Chris Maina <mainachrisw@gmail.com>
2021-12-21 10:08:48 +0300

    fix: remove access-control header

--- controlers/payments.controllers.ts
+++ controlers/payments.controllers.ts
@@ -152,7 +152,6 @@ router.post('/lipanampesa', async (req: Request, res: Response, next: NextFuncti
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "https://hamisha-api.herokuapp.com",
         "Authorization": `Bearer ${token?.access_token}`
       }
     }
