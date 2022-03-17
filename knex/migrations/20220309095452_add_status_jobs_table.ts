a0bde0163338009a859ae678fd937f13129c6561
Chris Maina <mainachrisw@gmail.com>
2022-03-17 10:11:15 +0300

    fix: modify defaultTo value

--- knex/migrations/20220309095452_add_status_jobs_table.ts
+++ knex/migrations/20220309095452_add_status_jobs_table.ts
@@ -1,10 +1,9 @@
 import * as Knex from "knex";
-import { JOB_STATUS } from "../../common/interfaces";
 
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.table("jobs", tbl => {
-    tbl.string("status").notNullable().defaultTo(JOB_STATUS.ACTIVE);
+    tbl.string("status").notNullable().defaultTo("active");
   });
 }
 
