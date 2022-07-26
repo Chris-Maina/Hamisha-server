75fbc0021b5e117d1c791d996e1a77091e92a506
Chris Maina <mainachrisw@gmail.com>
2022-07-26 11:07:43 +0300

    ch: move console.log

--- controlers/payments.controllers.ts
+++ controlers/payments.controllers.ts
@@ -48,10 +48,9 @@ router.post('/lipanampesa', async (req: Request, res: Response, next: NextFuncti
 // Webhook to listen to B2C response
 router.post('/b2c', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log("b2c success", req.body);
 
     if (req.body.Result.ResultCode !== 0) throw new createHttpError.BadRequest(req.body.Result.ResultDesc);
-    
+    console.log("b2c success", req.body.Result.ResultParameters);
     // Create a payment record
     const payload: { [x: string]: any } = mapMpesaKeysToSnakeCase(req.body.Result.ResultParameters.ResultParameter || []);
     if (req.query.invoice_id && req.query.contract_id) {
