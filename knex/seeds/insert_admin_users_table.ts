be97f0b8e783f06c1340721dbf0c3119beae173b
Chris Maina <mainachrisw@gmail.com>
2023-04-12 08:36:21 +0300

    fix: update seed file

--- knex/seeds/insert_admin_users_table.ts
+++ knex/seeds/insert_admin_users_table.ts
@@ -4,9 +4,10 @@ export async function seed(knex: Knex): Promise<void> {
     // Inserts seed entries
     await knex("users").insert([
         { 
-          email: 'hamisha@gmail.com',
+          email: 'chris.maina@bebataka.co.ke',
           role: 'admin',
-          first_name: "Hamisha",
+          first_name: "Chris",
+          last_name: "Maina",
           phone_number: '254716271297',
           password: `$2y$10$iyifR/PLoLg92j6UnXn1X.1h0L6j4PiaeoZRq8yRDJW7a2ItilM2y`
         },
